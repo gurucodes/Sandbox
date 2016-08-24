9bdae7297d35b9890987dc7bb09a097309281b67
Guru <gurdevsingh.salhotra@my.jcu.edu.au>
2016-08-24 20:42:16 +1000

    Add name print in every second alphabet

--- oddName.py
+++ oddName.py
@@ -1,1 +1,15 @@
 """Gurdev"""
+
+try:
+    name = input("Enter your name")
+    if not name:
+        raise ValueError('empty string')
+except ValueError as empty:
+    print(empty)
+
+for i in range(0,len(name),2):
+    print(name[i])
+
+
+
+
